eb075c12ae5019cfeb1302aed32f6912328bd336
guilhermepiccioni <guilhermepiccioni@yahoo.com>
2022-04-05 14:30:35 -0300

    Added error handling.

--- get_action.py
+++ get_action.py
@@ -13,25 +13,29 @@ class Actions:
 
     def get_all_actions(self):
 
-        for company in self.table_tickers["Company"]:
-            if company[0] == "^":
-                print(company)
-                indices = web.DataReader(f'{company}',
-                                         data_source=f'{self.local_web}',
-                                         start=f'{self.start_action}',
-                                         end=f'{self.end_action}')
-                indices["Adj Close"].plot(figsize=(10, 5), lw=2, ls='-.')
-                plt.title(f'{company}')
-                plt.show()
-            else:
-                print(company)
-                action = web.DataReader(f'{company}.SA',
-                                        data_source=f'{self.local_web}',
-                                        start=f'{self.start_action}',
-                                        end=f'{self.end_action}')
-                action["Adj Close"].plot(figsize=(10, 5), lw=2, ls='--')
-                plt.title(f'{company}')
-                plt.show()
+        try:
+            for company in self.table_tickers["Company"]:
+                if company[0] == "^":
+                    print(company)
+                    indices = web.DataReader(f'{company}',
+                                             data_source=f'{self.local_web}',
+                                             start=f'{self.start_action}',
+                                             end=f'{self.end_action}')
+                    indices["Adj Close"].plot(figsize=(10, 5), lw=2, ls='-.')
+                    plt.title(f'{company}')
+                    plt.show()
+                else:
+                    print(company)
+                    action = web.DataReader(f'{company}.SA',
+                                            data_source=f'{self.local_web}',
+                                            start=f'{self.start_action}',
+                                            end=f'{self.end_action}')
+                    action["Adj Close"].plot(figsize=(10, 5), lw=2, ls='--')
+                    plt.title(f'{company}')
+                    plt.show()
+
+        except Exception as err:
+            return f'Erro: {err}'
 
 
 if __name__ == '__main__':
